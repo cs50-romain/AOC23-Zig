e3d3c860ec1ce4b07d725840f215761030f672ac
cs50-romain <cs50.git.romain@gmail.com>
2024-05-22 21:50:49 -0400

    finished day 1 part 1

--- 1/main.zig
+++ 1/main.zig
@@ -2,6 +2,7 @@ const std = @import("std");
 const fs = std.fs;
 
 pub fn main() !void {
+    var result: u32 = 0;
     var gpa = std.heap.GeneralPurposeAllocator(.{}){};
     defer _ = gpa.deinit();
     const allocator = gpa.allocator();
@@ -16,18 +17,40 @@ pub fn main() !void {
     defer line.deinit();
 
     const writer = line.writer();
-    var line_no: usize = 1;
-    while (reader.streamUntilDelimiter(writer, '\n', null)) : (line_no += 1) {
+    while (reader.streamUntilDelimiter(writer, '\n', null)) {
         defer line.clearRetainingCapacity();
+        const nums = readLine(line.items);
+        result += nums[0] + nums[1];
 
-        std.debug.print("line number: {d} - line: {s}\n", .{ line_no, line.items });
+        //std.debug.print("line: {s}\n", .{line.items});
     } else |err| switch (err) {
         error.EndOfStream => {},
         else => return err,
     }
+    std.debug.print("Result: {d}\n", .{result});
 }
 
-fn readLine(reader: anytype, buffer: []u8) ![]const u8 {
-    const line = try reader.readUntilDelimiterOrEof(buffer, '\n');
-    return line;
+fn readLine(line: []u8) [2]u8 {
+    var nums_in_line: [10]u8 = undefined;
+    var idx: u8 = 0;
+    const nums = [10]u8{ 48, 49, 50, 51, 52, 53, 54, 55, 56, 57 };
+    for (line) |char| {
+        for (nums) |num| {
+            if (char == num) {
+                nums_in_line[idx] = char - 48;
+                idx += 1;
+            }
+        }
+    }
+
+    var result = [2]u8{ 0, 0 };
+
+    result[0] = nums_in_line[0] * 10;
+    if (nums_in_line.len > 1) {
+        result[1] = nums_in_line[idx - 1];
+    }
+
+    std.debug.print("{d}", .{result});
+
+    return result;
 }
